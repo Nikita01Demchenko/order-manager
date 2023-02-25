044083daa716a6c234ca24ca654007f865607196
Nikita Demchenko <nikita@MacBook-Air-Nikita.local>
2023-02-25 15:07:48 +0300

    Init commit

---
+++ src/main/java/com/pet/ordermanager/interfaces/OrderManager.java
@@ -0,0 +1,11 @@
+package com.pet.ordermanager.interfaces;
+
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ */
+@Service
+public interface OrderManager {
+
+}
---
+++ src/main/java/com/pet/ordermanager/model/dto/Order.java
@@ -0,0 +1,7 @@
+package com.pet.ordermanager.model.dto;
+
+import lombok.Data;
+
+@Data
+public class Order {
+}
